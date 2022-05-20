b393518b3f3e17d621a331b93825ef8612d0aeb7
unknown <bunt88@gmail.com>
2022-05-20 10:46:06 -0600

    Created local storage functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,43 @@
 
 // Use Moment.js to set the current date at the top of the app
-var today = moment().format("dddd, MMMM Do, YYYY");
+var today = moment().format("dddd, MMMM Do, YYYY, h:mm a");
 $("#currentDay").text(today);
 
+var currentHour = moment().hour();
+
+// for (i = 0; i < 18; i++) {
+// if (currentHour[i] = hour) {
+// addclass("present")
+// }
+
+// for (i = 0; i < 18; i++) {
+// if (currentHour[i] > hour) {
+// addclass("past")
+// }
+// }
+
+
+// Set items in local storage
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+        var task = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, task);
+    })
+    // Get items from local storage
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour18 .description").val(localStorage.getItem("hour18"));
+
+
+
+})
